3888e5fdf2a685e3e52389b2ec5666e1ffd71a6f
Ade <62720742+Ade0211@users.noreply.github.com>
2020-09-29 20:58:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,61 +1,9 @@
 //You can edit ALL of the code here
-
-// using the dropdown selector to go directly to the movie picked
-////////////////////////////////////////
-
-let selection = document.querySelector("#select");
-selection.addEventListener("change", () => {
-  let searchBar = document.getElementById("search-bar");
-  console.log(selection.options[selection.selectedIndex].text.split("-"));
-  searchBar.value = selection.options[selection.selectedIndex].text.split(
-    " - "
-  )[1];
-  search_Movies();
-  document.getElementById("back-link").style.visibility = "visible ";
-});
-
-// creating a search-bar for the movies
-/////////////////////////////
-
-function search_Movies(moviePage, episodes) {
-  let counter = 0;
-  let input = document.getElementById("search-bar").value;
-  input = input.toLowerCase();
-  let movies = document.querySelectorAll(".features");
-
-  let PageForMovies = document.querySelectorAll(".PageForMovies");
-
-  PageForMovies.forEach((moviePage) => {
-    if (moviePage.innerHTML.toLowerCase().includes(input)) {
-      counter++;
-      moviePage.style.display = "";
-    } else {
-      moviePage.style.display = "none";
-    }
-  });
-
-  movies.forEach((episode) => {
-    if (episode.innerHTML.toLowerCase().includes(input)) {
-      counter++;
-      episode.style.display = "";
-    } else {
-      episode.style.display = "none";
-    }
-  });
-  if (PageForMovies.length) {
-    document.getElementById("charNum").textContent = `found ${counter} shows`;
-  } else {
-    document.getElementById(
-      "charNum"
-    ).textContent = `Displaying ${counter} /${movies.length}`;
-  }
-}
 // Movie Set Up
 ////////////////
 
 function setup() {
   let allTheMovies = getAllShows().filter((item) => item.id != 1127);
-  console.log(allTheMovies);
   allTheMovies.sort((a, b) => {
     if (a.name.toLowerCase() > b.name.toLowerCase()) {
       return 1;
@@ -65,7 +13,6 @@ function setup() {
       return 0;
     }
   });
-  //console.log(allTheMovies);
   let movieChoice = document.getElementById("select2");
   for (let i = 0; i < allTheMovies.length; i++) {
     let movieOptions = document.createElement("option");
@@ -83,152 +30,15 @@ function setup() {
   makePageForMovies(allTheMovies);
 }
 
-// get Seasons
-///////////////////
-function makePageForSeasons(movieSeason) {
-  console.log(movieSeason);
-  const rootElem = document.getElementById("root");
-  // loop through the array of movies
-  for (let i = 0; i < movieSeason.length; i++) {
-    let message = movieSeason[i].summary;
-    let image = movieSeason[i].image.medium;
-    // console.log(image);
-    // console.log(message);
-    if (message) {
-      let char_limit = 100;
-      console.log(message.length);
-      if (message.length < char_limit) {
-        message = "<div> " + message + "</div>";
-        console.log(message);
-      } else
-        message =
-          "<div>" +
-          message.substr(0, char_limit) +
-          '<span class="long-text">' +
-          message.substr(char_limit) +
-          '</span><span class="text-dots">...</span><span class="show-more-button" data-more="0">Read More</span></div>';
-    } else {
-      message = "";
-    }
-    let SeasonDiv = document.createElement("div");
-    SeasonDiv.className = "seasons";
-
-    SeasonDiv.innerHTML = ` 
-                <div id = "moviePage">
-                <div class ="season"><p>Season ${
-                  movieSeason[i].number
-                }</p></div>
-                <img id="MovieImage" src="${
-                  movieSeason[i].image ? image : ""
-                }" alt="No Image Found">
-                <div id ="summary"><p>${message}</p></div>
-                </div>`;
-    console.log(SeasonDiv);
-    rootElem.appendChild(SeasonDiv);
-    //////////////////
-    let dots = SeasonDiv.querySelector(".text-dots");
-    let longText = SeasonDiv.querySelector(".long-text");
-    longText ? (longText.style.display = "none") : NaN;
-    let showMoreButton = SeasonDiv.querySelector(".show-more-button");
-
-    showMoreButton
-      ? showMoreButton.addEventListener("click", function () {
-          // If text is shown less, then show complete
-          if (this.getAttribute("data-more") == 0) {
-            this.setAttribute("data-more", 1);
-            this.style.display = "block";
-            this.innerHTML = "Read Less";
-
-            dots.style.display = "none";
-            longText.style.display = "inline";
-          }
-          // If text is shown complete, then show less
-          else if (this.getAttribute("data-more") == 1) {
-            this.setAttribute("data-more", 0);
-            this.style.display = "inline";
-            this.innerHTML = "Read More";
-            dots.style.display = "inline";
-            longText.style.display = "none";
-          }
-        })
-      : NaN;
-    ////////////////
-    movieSeason[i].image?  document.getElementById("select-season").addEventListener("change", (event) => {
-      const rootElem = document.getElementById("root");
-      let seasons = rootElem.querySelectorAll(".seasons");
-      seasons.forEach((item, i) => i? rootElem.removeChild(item): NaN);
-      document.body.appendChild(rootElem);
-      console.log(event.target.value);
-      makePageForSeasons(event.target.value);
-      
-    }): " ";
-    SeasonSelector = document.getElementById('select-season');
-    var opt = document.createElement('option');
-    // opt.value = movieSeason[i].number;
-    opt.innerHTML = `<p>Season ${movieSeason[i].number}</p>`;
-    SeasonSelector.appendChild(opt);
-    console.log(SeasonSelector);
-
-    ////////////////
-    SeasonDiv.addEventListener("click", function (event) {
-      // alert(movieSeason[i].id);
-      fetch(`https://api.tvmaze.com/shows/${movieSeason[i].id}/episodes`)
-        .then(function (response) {
-          return response.json();
-        })
-        .then((allEpisodes) => {
-          console.log(allEpisodes);
-          makePageForEpisodes(
-            allEpisodes.filter((item) => item.season === movieSeason[i].number)
-          );
-        })
-        .catch((error) => console.log(error));
-    });
-  }
-}
-
-
-/////////
-
-
-//Episodes ID
-function getEpisodes(showID) {
-  let rootElem = document.getElementById("root");
-  document.body.removeChild(rootElem);
-  rootElem = document.createElement("div");
-  rootElem.id = "root";
-  let episodeSelector = document.getElementById("select");
-  episodeSelector.style.visibility = "visible";
-  episodeSelector
-    .querySelectorAll("option")
-    .forEach((item, i) => (i ? episodeSelector.removeChild(item) : NaN));
-  document.body.appendChild(rootElem);
-
-  fetch(`https://api.tvmaze.com/shows/${showID}/seasons`)
-    .then(function (response) {
-      return response.json();
-    })
-    .then((allSeasons) => makePageForSeasons(allSeasons))
-    .catch((err) => {
-      console.log(err);
-      fetch(`https://api.tvmaze.com/shows/${showID}/episodes`)
-        .then(function (response) {
-          return response.json();
-        })
-        .then((allEpisodes) => {
-          makePageForEpisodes(allEpisodes);
-        })
-        .catch((error) => console.log(error));
-    });
-}
-
-
-
 // get allShows
 ///////////////////
 
 const makePageForMovies = (movieList) => {
   const rootElem = document.getElementById("root");
+    let SeasonSelector = document.getElementById('select-season');
+  let seasonOptions = SeasonSelector.querySelectorAll("option");
+
+  seasonOptions.forEach((item, ind) => ind ? SeasonSelector.removeChild(item):NaN);
   // loop through the array of movies
   for (let i = 0; i < movieList.length; i++) {
     // creating a new div for every movie
@@ -328,12 +138,108 @@ const makePageForMovies = (movieList) => {
     "charNum"
   ).textContent = `found ${movieList.length} shows`;
 };
-//  getting episodeList
-//////////////////
-document.getElementById("select2").addEventListener("change", (event) => {
-  console.log(event.target.value);
-  getEpisodes(event.target.value);
-});
+
+
+// get Seasons
+///////////////////
+function makePageForSeasons(movieSeason) {
+  console.log(movieSeason);
+  const rootElem = document.getElementById("root");
+  let SeasonSelector = document.getElementById('select-season');
+  let seasonOptions = SeasonSelector.querySelectorAll("option");
+  // alert("rrr");
+  seasonOptions.forEach((item, ind) => ind ? SeasonSelector.removeChild(item):NaN);
+
+  // loop through the array of movies
+  for (let i = 0; i < movieSeason.length; i++) {
+    let message = movieSeason[i].summary;
+    let image = movieSeason[i].image.medium;
+    if (message) {
+      let char_limit = 100;
+      console.log(message.length);
+      if (message.length < char_limit) {
+        message = "<div> " + message + "</div>";
+        console.log(message);
+      } else
+        message =
+          "<div>" +
+          message.substr(0, char_limit) +
+          '<span class="long-text">' +
+          message.substr(char_limit) +
+          '</span><span class="text-dots">...</span><span class="show-more-button" data-more="0">Read More</span></div>';
+    } else {
+      message = "";
+    }
+    let SeasonDiv = document.createElement("div");
+    SeasonDiv.className = "seasons";
+
+    SeasonDiv.innerHTML = ` 
+                <div id = "moviePage">
+                <div class ="season"><p>Season ${
+                  movieSeason[i].number
+                }</p></div>
+                <img id="MovieImage" src="${
+                  movieSeason[i].image ? image : ""
+                }" alt="No Image Found">
+                <div id ="summary"><p>${message}</p></div>
+                </div>`;
+    console.log(SeasonDiv);
+    rootElem.appendChild(SeasonDiv);
+    //////////////////
+    let dots = SeasonDiv.querySelector(".text-dots");
+    let longText = SeasonDiv.querySelector(".long-text");
+    longText ? (longText.style.display = "none") : NaN;
+    let showMoreButton = SeasonDiv.querySelector(".show-more-button");
+
+    showMoreButton
+      ? showMoreButton.addEventListener("click", function () {
+          // If text is shown less, then show complete
+          if (this.getAttribute("data-more") == 0) {
+            this.setAttribute("data-more", 1);
+            this.style.display = "block";
+            this.innerHTML = "Read Less";
+
+            dots.style.display = "none";
+            longText.style.display = "inline";
+          }
+          // If text is shown complete, then show less
+          else if (this.getAttribute("data-more") == 1) {
+            this.setAttribute("data-more", 0);
+            this.style.display = "inline";
+            this.innerHTML = "Read More";
+            dots.style.display = "inline";
+            longText.style.display = "none";
+          }
+        })
+      : NaN;
+    ////////////////
+    movieSeason[i].image?  document.getElementById("select-season").addEventListener("change", (event) => {
+      const rootElem = document.getElementById("root");
+      let seasons = rootElem.querySelectorAll(".seasons");
+      seasons.forEach((item, i) => (i+1) !=event.target.value ? item.style.display = "none": item.style.display = "");
+    }): " ";
+    SeasonSelector = document.getElementById('select-season');
+    var opt = document.createElement('option');
+    opt.value = movieSeason[i].number;
+    opt.innerHTML = `<p>Season ${movieSeason[i].number}</p>`;
+    SeasonSelector.appendChild(opt);
+    SeasonDiv.addEventListener("click", function (event) {
+      fetch(`https://api.tvmaze.com/shows/${movieSeason[i].id}/episodes`)
+        .then(function (response) {
+          return response.json();
+        })
+        .then((allEpisodes) => {
+          console.log(allEpisodes);
+          makePageForEpisodes(
+            allEpisodes.filter((item) => item.season === movieSeason[i].number)
+          );
+        })
+        .catch((error) => console.log(error));
+    });
+  }
+}
+
+/////Episodes///////////
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
@@ -359,11 +265,8 @@ function makePageForEpisodes(episodeList) {
       message = "";
     }
     let season = episodeList[i].season;
-    console.log(season);
     let episodeNumber = episodeList[i].number;
-    console.log(episodeNumber);
     let image = episodeList[i].image;
-
     // creating a new div for every movie
     const movieDiv = document.createElement("div");
     // giving a className to the movie div
@@ -375,7 +278,6 @@ function makePageForEpisodes(episodeList) {
       episodeList[i].name
     }`;
     option.value = i;
-    console.log(option);
     document.getElementById("select").appendChild(option);
     image
       ? (movieDiv.innerHTML = `<p id="header">${episodeList[i].name} - S${
@@ -392,6 +294,7 @@ function makePageForEpisodes(episodeList) {
       <p>
       ${message}
       </p>`);
+
 
     let dots = movieDiv.querySelector(".text-dots");
     let longText = movieDiv.querySelector(".long-text");
@@ -427,6 +330,97 @@ function makePageForEpisodes(episodeList) {
     ).textContent = `Displaying ${episodeList.length} / ${episodeList.length}`;
   }
 }
+
+//Episodes ID///
+function getEpisodes(showID) {
+  let rootElem = document.getElementById("root");
+  document.body.removeChild(rootElem);
+  rootElem = document.createElement("div");
+  rootElem.id = "root";
+  let episodeSelector = document.getElementById("select");
+  episodeSelector.style.visibility = "visible";
+  episodeSelector
+    .querySelectorAll("option")
+    .forEach((item, i) => (i ? episodeSelector.removeChild(item) : NaN));
+  document.body.appendChild(rootElem);
+
+  fetch(`https://api.tvmaze.com/shows/${showID}/seasons`)
+    .then(function (response) {
+      return response.json();
+    })
+    .then((allSeasons) => makePageForSeasons(allSeasons))
+    .catch((err) => {
+      console.log(err);
+      fetch(`https://api.tvmaze.com/shows/${showID}/episodes`)
+        .then(function (response) {
+          return response.json();
+        })
+        .then((allEpisodes) => {
+          console.log(allEpisodes);
+          makePageForEpisodes(allEpisodes);
+        })
+        .catch((error) => console.log(error));
+    });
+}
+
+/////Movie Search/////
+function search_Movies(moviePage, episodes) {
+  let counter = 0;
+  let input = document.getElementById("search-bar").value;
+  input = input.toLowerCase();
+  let movies = document.querySelectorAll(".features");
+
+  let PageForMovies = document.querySelectorAll(".PageForMovies");
+
+  PageForMovies.forEach((moviePage) => {
+    if (moviePage.innerHTML.toLowerCase().includes(input)) {
+      counter++;
+      moviePage.style.display = "";
+    } else {
+      moviePage.style.display = "none";
+    }
+  });
+
+  movies.forEach((episode) => {
+    if (episode.innerHTML.toLowerCase().includes(input)) {
+      counter++;
+      episode.style.display = "";
+    } else {
+      episode.style.display = "none";
+    }
+  });
+  if (PageForMovies.length) {
+    document.getElementById("charNum").textContent = `found ${counter} shows`;
+  } else {
+    document.getElementById(
+      "charNum"
+    ).textContent = `Displaying ${counter} /${movies.length}`;
+  }
+}
+
+// dropdown selector Eventlistener for Episodes //
+////////////////////////////////////////
+let selection = document.querySelector("#select");
+selection.addEventListener("change", () => {
+  alert("hi")
+  let searchBar = document.getElementById("search-bar");
+  console.log(selection.options[selection.selectedIndex].text.split("-"));
+  searchBar.value = selection.options[selection.selectedIndex].text.split(
+    " - "
+  )[1];
+  search_Movies();
+  document.getElementById("back-link").style.visibility = "visible ";
+});
+
+
+// episodeList EventListeners///
+//////////////////
+document.getElementById("select2").addEventListener("change", (event) => {
+  console.log(event.target.value);
+  getEpisodes(event.target.value);
+});
+
+
 window.onload = setup;
 
 // A link to the original data
